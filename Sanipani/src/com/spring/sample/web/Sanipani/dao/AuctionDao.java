41022fa88575a436da5931b52195c50b545774b7
student <student@DESKTOP-JKNG9VC>
2016-11-21 15:09:31 +0900

    2016-11-21 15:09 임섬김 경매게시판 추가

--- Sanipani/src/com/spring/sample/web/Sanipani/dao/AuctionDao.java
+++ Sanipani/src/com/spring/sample/web/Sanipani/dao/AuctionDao.java
@@ -1,5 +1,28 @@
 package com.spring.sample.web.Sanipani.dao;
 
-public class AuctionDao {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+@Repository
+public class AuctionDao implements IAuctionDao {
+
+	@Autowired
+	public SqlMapClient sqlMapClient;
+	
+	@Override
+	public int getAuctionCount(HashMap<String, String> params) throws Throwable {
+		return (int) sqlMapClient.queryForObject("AuctionBoard.getAuctionCount", params);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public ArrayList<HashMap<String, String>> getAuction(HashMap<String, String> params) throws Throwable {
+		return (ArrayList<HashMap<String, String>>) sqlMapClient.queryForList("AuctionBoard.getAuction", params);
+	}
 
 }
